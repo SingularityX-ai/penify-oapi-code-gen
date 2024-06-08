ab0184265dc09e91e64264bc73829cd20eba8063
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-06-09 02:08:30 +0530

    chore: Update package name to penify-oapi-codegen and improve command-line argument handling

--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@
 const OpenAPIHelper = require('./OpenAPIHelper');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 
-// Get the OpenAPI JSON path from the command-line arguments
-const openAPIPath = process.argv[2];
-const language = process.argv[3] || null; // Language input (optional)
-const variant = process.argv[4] || null;  // Variant input (optional)
+// Parse command-line arguments
+const argv = yargs(hideBin(process.argv))
+  .usage('Usage: penify-oapi-codegen -s <source> -l <language> -v <variant> -o <output>')
+  .option('s', {
+    alias: 'source',
+    describe: 'Path to the OpenAPI JSON file',
+    type: 'string',
+    demandOption: true
+  })
+  .option('l', {
+    alias: 'language',
+    describe: 'Language input (optional)',
+    type: 'string'
+  })
+  .option('v', {
+    alias: 'variant',
+    describe: 'Variant input (optional)',
+    type: 'string'
+  })
+  .option('o', {
+    alias: 'output',
+    describe: 'Output file path (optional)',
+    type: 'string'
+  })
+  .help()
+  .argv;
+
+const openAPIPath = argv.s;
+const language = argv.l || null;
+const variant = argv.v || null;
+const outputAPIPath = argv.o || path.resolve(`${path.basename(openAPIPath)}_with_code.json`);
 
 if (!openAPIPath) {
   console.error('Please provide the path to the OpenAPI JSON file.');
@@ -19,7 +48,7 @@ const file_name = path.basename(resolvedOpenAPIPath);
 const guid = uuidv4();
 
 const postmanOutputPath = path.resolve(`/tmp/openapi_schema_postman_${guid}.json`);
-const outputAPIPath = path.resolve(`${file_name}_with_code.json`);
+
 console.log("Executing step 1.");
 OpenAPIHelper.convertOpenAPIToPostman(resolvedOpenAPIPath, postmanOutputPath);
 console.log("Executing step 2.");
